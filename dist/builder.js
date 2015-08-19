edba5546f06e8ae558138069cc469b8d85dcecfd
Eric Martin <ericmartin@yahoo-inc.com>
2015-08-19 03:37:53 +0000

    Builder updates

--- dist/builder.js
+++ dist/builder.js
@@ -2,61 +2,67 @@ var upgrader = require('upgrader');
 
 module.exports = function(creep) {
 
-    if(creep.carry.energy == 0) {
+    if(creep.carry.energy === 0) {
       creep.moveTo(creep.room.storage);
       creep.room.storage.transferEnergy(creep);
-    } else {
-        if(creep.memory.myTargetId == null) {
-            creep.memory.myTargetId = newTarget(creep);
-            console.log('New Target for ' + creep.name + ': ' + creep.memory.myTargetId);
-        }
+      return;
+    } 
 
-        var target = Game.getObjectById(creep.memory.myTargetId);
-        if(target == null) {
-            console.log('No target, temporary upgarder');
-            upgrader(creep);
-            return;
-        }
+    if(creep.memory.myTargetId == null) {
+        creep.memory.myTargetId = newTarget(creep);
+        console.log('New Target for ' + creep.name + ': ' + creep.memory.myTargetId);
+    }
 
-      var res = creep.moveTo(target);
+    var target = Game.getObjectById(creep.memory.myTargetId);
+    if(target === null) {
+        console.log('No target, temporary upgarder');
+        upgrader(creep);
+        return;
+    }
 
-            // Is a construction site
-      if(target.progress != null) {
-          if (target.progress < target.progressTotal) {
-              if (creep.pos.isNearTo(target)) {
-                  creep.say(completedPretty(target) + '%');
-                  creep.build(target);
-              }
-          } else {
-              console.log('clearing target ' + creep.name);
-              creep.memory.myTargetId = null;
-          }
-          return;
-      }
+    if (!creep.pos.isNearTo(target)) {
+        creep.moveTo(target);
+    }
 
-        if (needsRepair(target)) {
+    // Is a construction site
+    if(target.progress >= 0) {
+        if (target.progress < target.progressTotal) {
             if (creep.pos.isNearTo(target)) {
                 creep.say(completedPretty(target) + '%');
-                creep.repair(target);
+                creep.build(target);
             }
         } else {
-            console.log('clearing target ' + creep.name);
+            console.log('clearing target ' + creep.name + ' target: ' + target.structureType + ' ' + target.progress + '/' + target.progressTotal);
             creep.memory.myTargetId = null;
         }
-
+        return;
     }
+    
+    if (needsRepair(target)) {
+        if (creep.pos.isNearTo(target)) {
+            creep.say(completedPretty(target) + '%');
+            creep.repair(target);
+        }
+    } else {
+        console.log('clearing target ' + creep.name + ' target: ' + target.structureType + ' ' + target.hits + '/' + target.hitsMax);
+        creep.memory.myTargetId = null;
+    }
+
+    
 }
 
 function needsRepair(target) {
+    //console.log('needs repair? ' + target.hits + '/' + target.hitsMax);
     return target.hits < (target.hitsMax / 2);
 }
 
 function newTarget(creep) {
-    var constructionSites = creep.room.find(FIND_CONSTRUCTION_SITES);
-    var site = null;
+    var constructionSites = creep.room.find(FIND_CONSTRUCTION_SITES),
+        site = null,
+        target = null;
 
     if(constructionSites.length) {
-        site =  constructionSites[0];
+        site = constructionSites[0];
     }
 
     var structures = creep.room.find(FIND_STRUCTURES);
@@ -64,31 +70,30 @@ function newTarget(creep) {
 
     for(var i in structures) {
         var s = structures[i];
-        var target = null;
 
-        if (s.hits == null) {
+        if (s.hits === null) {
             continue;
         } else if (needsRepair(s)) {
-            if (s.structureType == STRUCTURE_RAMPART) {
-                if (target != null && target.structureType == STRUCTURE_RAMPART && target.hits < s.hits) {
+            if (s.structureType === STRUCTURE_RAMPART) {
+                if (target !== null && target.structureType == STRUCTURE_RAMPART && target.hits < s.hits) {
                     continue;
                 }
                 target = s;
-            } if (s.structureType == STRUCTURE_ROAD && ( target == null || target.structureType != STRUCTURE_RAMPART)) {
+            } if (s.structureType == STRUCTURE_ROAD && ( target === null || target.structureType != STRUCTURE_RAMPART)) {
                 target = s;
-            } if (target == null || s.structureType == STRUCTURE_WALL && (target == null || [STRUCTURE_RAMPART, STRUCTURE_ROAD].indexOf(target.structureType) == -1)) {
+            } if (target === null || s.structureType == STRUCTURE_WALL && (target === null || [STRUCTURE_RAMPART, STRUCTURE_ROAD].indexOf(target.structureType) == -1)) {
                 target = s;
             }
         }
     }
 
-        if (target == null && site != null) {
+        if (target === null && site !== null) {
             return site.id;
-        } else if (target != null && target.hits < (target.hitsMax / 4)) {
+        } else if (target !== null && target.hits < (target.hitsMax / 4)) {
             return target.id;
-        } else if (site != null) {
-            return site.id
-        } else if (target != null) {
+        } else if (site !== null) {
+            return site.id;
+        } else if (target !== null) {
             return target.id;
         }
         console.log('failed to find target');
@@ -96,7 +101,7 @@ function newTarget(creep) {
 }
 
 function completedPretty(target) {
-    if (target.hits != null) {
+    if (target.hits !== null) {
         return parseInt((target.hits / target.hitsMax) * 100);
     }
     return parseInt((target.progress / target.progressTotal) * 100);
